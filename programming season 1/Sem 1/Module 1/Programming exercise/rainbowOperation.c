c5aff7a66ba4b4c6945dd76bf8316f263dbf60b2
Nicholas Noly C. dela Torre <88265052+Nicktacku@users.noreply.github.com>
2021-11-02 12:50:40 +0800

    upload

---
+++ programming season 1/Sem 1/Module 1/Programming exercise/rainbowOperation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int operationExplosion(int a, int b, int c);
+
+int main() {
+    int a, b, c;
+
+    printf("sdpqs2 displayer\n");
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+    printf("Enter third number: ");
+    scanf("%d", &c);
+
+    operationExplosion(a, b, c);
+    return 0;
+}
+
+int operationExplosion(int a, int b, int c){
+    //computes the different kind of operations
+    printf("\nsum: %d\ndifference: %d\nproduct: %d\nquotient: %d\nsum of squares: %d",
+    a + b + c, a - b - c, a * b * c, a / b / c, (a * a) + (b * b) + (c * c)); // was done in 1 print because will result in error if seperated
+}
