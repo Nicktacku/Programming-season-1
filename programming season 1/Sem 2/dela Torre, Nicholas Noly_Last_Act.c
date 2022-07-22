6494e9bdf6d31fd4851f260a33c6a9db92aa8d0c
Nicholas Noly C. dela Torre <88265052+Nicktacku@users.noreply.github.com>
2022-07-22 21:03:57 +0800

    menu design done
    logic of code is decent, the design in menu is done. next to be designed is the create function

--- programming season 1/Sem 2/dela Torre, Nicholas Noly_Last_Act.c
+++ programming season 1/Sem 2/dela Torre, Nicholas Noly_Last_Act.c
@@ -6,44 +6,139 @@ void display();
 void search();
 void back_up();
 int total();
+int total_search(char *date);
 
 int main()
 {
-    int choice;
-    char end = 'y';
+    int choice = 0;
+    char press;
+    char end = 'n';
 
-    printf("Aice order recorder\n\n");
-    printf("Menu:\n\n");
-    printf("Create new record\n");
-    printf("Add new order\n");
-    printf("Display all order\n");
-    printf("Search\n");
-    printf("Back Up\n");
-    printf("Exit\n");
+    line2(2, 2, 80, 6);
+    line2(2, 2, 80, 24);
+    printxy(4, 4, "Aice Order Recorder");
 
-    printf("\n\nEnter choice: ");
-    scanf("%d", &choice);
+    gotoxy(77, 4);
+    putchar('X');
+    gotoxy(73, 4);
+    putchar(254);
+    gotoxy(69, 4);
+    putchar(196);
 
-    if (choice == 1)
+    printxy(4, 8, "This is a recorder for the orders of your aice branch");
+    printxy(4, 9, "it creates a file under the name \"Records\"");
+    printxy(4, 11, "(use \"w\" and \"s\" to navigate the menu and \"e\" to choose it)");
+
+    printxy(4, 13, "Menu:");
+    printxy(4, 14, "Create new record");
+    printxy(4, 15, "Add new order");
+    printxy(4, 16, "Display all order");
+    printxy(4, 17, "Search");
+    printxy(4, 18, "Back Up");
+    printxy(4, 19, "Exit");
+
+    gotoxy(22, 14);
+    putchar(174);
+    gotoxy(4, 22);
+
+    while (end != 'y')
     {
-        create();
+        press = getch();
+
+        if (press == 'w')
+        {
+            choice--;
+            if (choice == -1)
+            {
+                choice = 5;
+            }
+        }
+        else if (press == 's')
+        {
+            choice++;
+            if (choice == 6)
+            {
+                choice = 0;
+            }
+        }
+        else if (press == 'e')
+        {
+            if (choice == 0)
+            {
+                create();
+            }
+            else if (choice == 1)
+            {
+                order();
+            }
+            else if (choice == 2)
+            {
+                display();
+            }
+            else if (choice == 3)
+            {
+                search();
+            }
+            else if (choice == 4)
+            {
+                back_up();
+            }
+            else if (choice == 5)
+            {
+                end = 'y';
+            }
+        }
+
+        if (choice == 0)
+        {
+            clearportion(9, 19, 12, 19);
+            clearportion(18, 15, 22, 15);
+            gotoxy(22, 14);
+            putchar(174);
+            gotoxy(4, 22);
+        }
+        else if (choice == 1)
+        {
+            clearportion(22, 14, 25, 14);
+            clearportion(22, 16, 25, 16);
+            gotoxy(18, 15);
+            putchar(174);
+            gotoxy(4, 22);
+        }
+        else if (choice == 2)
+        {
+            clearportion(18, 15, 22, 15);
+            clearportion(11, 17, 14, 17);
+            gotoxy(22, 16);
+            putchar(174);
+            gotoxy(4, 22);
+        }
+        else if (choice == 3)
+        {
+            clearportion(22, 16, 25, 16);
+            clearportion(12, 18, 15, 18);
+            gotoxy(11, 17);
+            putchar(174);
+            gotoxy(4, 22);
+        }
+        else if (choice == 4)
+        {
+            clearportion(11, 17, 14, 17);
+            clearportion(9, 19, 12, 19);
+            gotoxy(12, 18);
+            putchar(174);
+            gotoxy(4, 22);
+        }
+        else if (choice == 5)
+        {
+            clearportion(12, 18, 15, 18);
+            clearportion(22, 14, 25, 14);
+            gotoxy(9, 19);
+            putchar(174);
+            gotoxy(4, 22);
+        }
     }
-    else if (choice == 2)
-    {
-        order();
-    }
-    else if (choice == 3)
-    {
-        display();
-    }
-    else if (choice == 4)
-    {
-        search();
-    }
-    else if (choice == 5)
-    {
-        back_up();
-    }
+    gotoxy(0, 26);
     return 0;
 }
 
@@ -538,10 +633,12 @@ void search()
     char *split;
     char line[255];
     char product[30];
-    int it = 0, ctr = 0, pr = 1, counter = 0, pg = 1, pgctr;
+    int it = 0, ctr = 0, pr = 1, counter = 0, page = 1, pgctr = 0, dt = 0, total;
 
     printf("Enter date to be searched (Format: mm/dd/yyyy): ");
     scanf("%s", date);
+
+    total = total_search(date);
 
     while (fscanf(ic_record, "%s", line) != EOF)
     {
@@ -671,6 +768,16 @@ void search()
                     if (counter == 6)
                     {
                         counter = 0;
+                        pgctr++;
+
+                        if (pgctr == 3)
+                        {
+                            getch();
+                            system("cls");
+                            page++;
+                            printf("\npage: %d", page);
+                            printf(" of %d", total);
+                        }
                     }
                 }
 
@@ -680,11 +787,28 @@ void search()
 
         split = strtok(line, " ");
 
-        if (strcmp(date, split) == 0)
+        if (dt == 1)
         {
-            ctr = 1;
+            if (strcmp(date, line) == 0)
+            {
+                printf("\ndate: %s", date);
+                printf("\npage: %d", page);
+                printf(" of %d", total);
+                ctr = 1;
+                dt = 0;
+            }
+            else
+            {
+                dt = 0;
+            }
         }
-        else if (strcmp(split, "EOR") == 0)
+
+        if (strcmp(split, "SOR") == 0)
+        {
+            dt = 1;
+        }
+
+        if (strcmp(split, "EOR") == 0)
         {
             ctr = 0;
         }
@@ -692,7 +816,6 @@ void search()
 
     if (it == 1)
     {
-        printf("\ndate: %s", date);
     }
     else
     {
@@ -710,5 +833,183 @@ void back_up()
     {
         fgets(line, 255, orig);
         fprintf(back_up, "%s", line);
+    }
+}
+
+int total_search(char *date)
+{
+    FILE *ic_record = fopen("Records.txt", "r");
+    char *split;
+    char line[255];
+    char product[30];
+    int it = 0, ctr = 0, pr = 1, counter = 0, page = 1, pgctr = 0, dt = 0, total;
+
+    while (fscanf(ic_record, "%s", line) != EOF)
+    {
+        split = strtok(line, " ");
+
+        if (ctr == 1 && strcmp(line, "EOR") != 0)
+        {
+            while (split != NULL)
+            {
+                it = 1;
+                if (counter == 0)
+                {
+                    if (strcmp(split, "1") == 0)
+                    {
+                        char ms[40] = "Milk Stick";
+                        strcpy(product, ms);
+                    }
+                    else if (strcmp(split, "2") == 0)
+                    {
+                        char wm[40] = "Watermelon";
+                        strcpy(product, wm);
+                    }
+                    else if (strcmp(split, "3") == 0)
+                    {
+                        char cst[40] = "Chocolate Stick";
+                        strcpy(product, cst);
+                    }
+                    else if (strcmp(split, "12") == 0)
+                    {
+                        char p[40] = "Pineapple";
+                        strcpy(product, p);
+                    }
+                    else if (strcmp(split, "4") == 0)
+                    {
+                        char mm[40] = "Milk Melon";
+                        strcpy(product, mm);
+                    }
+                    else if (strcmp(split, "5") == 0)
+                    {
+                        char cc[40] = "Coffee Crispy";
+                        strcpy(product, cc);
+                    }
+                    else if (strcmp(split, "6") == 0)
+                    {
+                        char s[40] = "Sweetcorn";
+                        strcpy(product, s);
+                    }
+                    else if (strcmp(split, "7") == 0)
+                    {
+                        char cs[40] = "Chocolate Sundae";
+                        strcpy(product, cs);
+                    }
+                    else if (strcmp(split, "18") == 0)
+                    {
+                        char ccp[40] = "Choco Cup";
+                        strcpy(product, ccp);
+                    }
+                    else if (strcmp(split, "20") == 0)
+                    {
+                        char ss[40] = "Strawberry Sundae";
+                        strcpy(product, ss);
+                    }
+                    else if (strcmp(split, "8") == 0)
+                    {
+                        char csp[40] = "Chocolate Crispy";
+                        strcpy(product, csp);
+                    }
+                    else if (strcmp(split, "13") == 0)
+                    {
+                        char scp[40] = "Strawberry Crispy";
+                        strcpy(product, scp);
+                    }
+                    else if (strcmp(split, "21") == 0)
+                    {
+                        char cck[40] = "Choco Cookies";
+                        strcpy(product, cck);
+                    }
+                    else if (strcmp(split, "9") == 0)
+                    {
+                        char scn[40] = "Strawberry Cone";
+                        strcpy(product, scn);
+                    }
+                    else if (strcmp(split, "10") == 0)
+                    {
+                        char cm[40] = "Choco Melt";
+                        strcpy(product, cm);
+                    }
+                    else if (strcmp(split, "11") == 0)
+                    {
+                        char as[40] = "Alpukat Strawberry";
+                        strcpy(product, as);
+                    }
+                    else if (strcmp(split, "17") == 0)
+                    {
+                        char cv[40] = "Choco Vanilla 2in1";
+                        strcpy(product, cv);
+                    }
+                    else if (strcmp(split, "14") == 0)
+                    {
+                        char ms[40] = "Mango Slush";
+                        strcpy(product, ms);
+                    }
+                    else if (strcmp(split, "15") == 0)
+                    {
+                        char vm[40] = "Vanilla Mochi";
+                        strcpy(product, vm);
+                    }
+                    else if (strcmp(split, "16") == 0)
+                    {
+                        char mc[40] = "Mochi Chocolate";
+                        strcpy(product, mc);
+                    }
+                    else if (strcmp(split, "19") == 0)
+                    {
+                        char ip[40] = "Ice Pop";
+                        strcpy(product, ip);
+                    }
+                    counter++;
+                }
+                else if (counter > 0)
+                {
+                    counter++;
+
+                    if (counter == 6)
+                    {
+                        counter = 0;
+                        pgctr++;
+
+                        if (pgctr == 3)
+                        {
+                            total++;
+                        }
+                    }
+                }
+
+                split = strtok(NULL, " ");
+            }
+        }
+
+        split = strtok(line, " ");
+
+        if (dt == 1)
+        {
+            if (strcmp(date, line) == 0)
+            {
+                ctr = 1;
+                dt = 0;
+            }
+            else
+            {
+                dt = 0;
+            }
+        }
+
+        if (strcmp(split, "SOR") == 0)
+        {
+            dt = 1;
+        }
+
+        if (strcmp(split, "EOR") == 0)
+        {
+            ctr = 0;
+        }
+    }
+
+    if (it == 1)
+    {
+        return total;
     }
 }
